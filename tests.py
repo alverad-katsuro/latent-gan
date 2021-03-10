4fb2c611963bc9786b4d0c375c74881c7970b243
kpsg824 <kpsg824@wks-df0fc0.seml.astrazeneca.net>
2021-03-10 01:38:26 +0100

    bugfix: test_chembl_baseline_model contained no indent

--- tests.py
+++ tests.py
@@ -21,18 +21,18 @@ class test_heteroencoder(unittest.TestCase):
                         "Model has encoded the same latent vector as two different SMILES")
     
     def test_chembl_baseline_model(self):
-    # The encoder/decoder pair should consistently have the same output for any specific input 
-    # This test checks the robustness of the baseline chembl model to encoder noise.
-    binary_mol = [Chem.rdchem.Mol.ToBinary(Chem.MolFromSmiles(self.reference_molecule))]
-    latent = self.model.transform(self.model.vectorize(binary_mol))
-    latent=latent.squeeze(0)
-    smiles, _ = self.model.predict(latent, temp=0)
-    # The same output molecule can have several corresponding smiles and the decoder is trained
-    # to output one of these. This canonicalization verifies that indeed the same molecule is
-    # produced every time
-    canonical_form = Chem.MolToSmiles(Chem.MolFromSmiles(smiles))
-    self.assertEqual(canonical_form,'CC(=O)Oc1ccccc1C(=O)O',
-                        "Decoder output does not match expected output of ChEMBL trained heteroencoder")
+        # The encoder/decoder pair should consistently have the same output for any specific input 
+        # This test checks the robustness of the baseline chembl model to encoder noise.
+        binary_mol = [Chem.rdchem.Mol.ToBinary(Chem.MolFromSmiles(self.reference_molecule))]
+        latent = self.model.transform(self.model.vectorize(binary_mol))
+        latent=latent.squeeze(0)
+        smiles, _ = self.model.predict(latent, temp=0)
+        # The same output molecule can have several corresponding smiles and the decoder is trained
+        # to output one of these. This canonicalization verifies that indeed the same molecule is
+        # produced every time
+        canonical_form = Chem.MolToSmiles(Chem.MolFromSmiles(smiles))
+        self.assertEqual(canonical_form,'CC(=O)Oc1ccccc1C(=O)O',
+                            "Decoder output does not match expected output of ChEMBL trained heteroencoder")
             
 
 
