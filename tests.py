be8caba6767cdcaddbbee801bdd0da25543c2b88
kpsg824 <kpsg824@wks-df0fc0.seml.astrazeneca.net>
2021-03-10 01:29:09 +0100

    testing: Refactored tests.py to have a common setUp for future tests

--- tests.py
+++ tests.py
@@ -5,19 +5,22 @@ import unittest
 
 
 class test_heteroencoder(unittest.TestCase):
-    def test_decoder(self):
+    def setUp(self):
         self.model = autoencoder.load_model(model_version='chembl')
-        test_aspirin = 'CC(=O)OC1=CC=CC=C1C(=O)O'
-        # The decoder is deterministic and should always generate the same output
-        binary_mol = [Chem.rdchem.Mol.ToBinary(Chem.MolFromSmiles(test_aspirin))]
-        latent = self.model.transform(self.model.vectorize(binary_mol))
+        self.reference_molecule= 'CC(=O)OC1=CC=CC=C1C(=O)O'
+        self.binarization= [Chem.rdchem.Mol.ToBinary(Chem.MolFromSmiles(self.reference_molecule))]
+        
+    def test_decoder_deterministic(self):
+        # The decoder is deterministic and should have no variance when decoding the same latent vector
+        # Note that Encoder has a noise component and this latent vector can vary between runs
+        latent = self.model.transform(self.model.vectorize(self.binarization))
         latent=latent.squeeze(0)
         first, _ = self.model.predict(latent, temp=0)
         second, _= self.model.predict(latent, temp=0)
         self.assertEqual(first,second, 
                         "Model has encoded the same latent vector as two different SMILES")
-       
-            
+    
+
 
 if __name__ == '__main__':
     unittest.main()
