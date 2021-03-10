95f5d24ad9ad9fbc618369b9b5746fccda540842
kpsg824 <kpsg824@wks-df0fc0.seml.astrazeneca.net>
2021-03-10 11:34:33 +0100

    bugfix: reverted optimization. Reason: short SMILES output strings share id

--- tests.py
+++ tests.py
@@ -9,20 +9,24 @@ class test_heteroencoder(unittest.TestCase):
     def setUp(self):
         path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'autoencoder/chembl_pretrained')
         self.model = ddc.DDC(model_name=path)
+        self.reference_molecule= 'CC(=O)OC1=CC=CC=C1C(=O)O'
+        self.binary_mol = [Chem.rdchem.Mol.ToBinary(Chem.MolFromSmiles(self.reference_molecule))]
 
         
     def test_decoder_different_obj(self):
         # Two calls of the same function should yield two different objects in memory
-        #  or the following tests have no meaning.
-        latent=np.random.rand(1,1,512)
+        #  or the following tests have no meaning.    
+        latent = self.model.transform(self.model.vectorize(self.binary_mol))
         latent=latent.squeeze(0)
         first, _ = self.model.predict(latent, temp=0)
         second, _= self.model.predict(latent, temp=0)
-        self.assertTrue(first is not second)    
+        a= id(first)
+        b= id(second)
+        self.assertTrue(a != b)    
         
     def test_decoder_deterministic(self):
         # The decoder is deterministic and should have no variance when decoding the same latent vector
-        latent=np.random.rand(1,1,512)
+        latent = self.model.transform(self.model.vectorize(self.binary_mol))
         latent=latent.squeeze(0)
         first, _ = self.model.predict(latent, temp=0)
         second, _= self.model.predict(latent, temp=0)
@@ -32,9 +36,8 @@ class test_heteroencoder(unittest.TestCase):
     def test_chembl_baseline_model(self):
         # The encoder/decoder pair should consistently have the same output for any specific input 
         # This test checks the robustness of the baseline chembl model to encoder noise.
-        self.reference_molecule= 'CC(=O)OC1=CC=CC=C1C(=O)O'
-        binary_mol = [Chem.rdchem.Mol.ToBinary(Chem.MolFromSmiles(self.reference_molecule))]
-        latent = self.model.transform(self.model.vectorize(binary_mol))
+
+        latent = self.model.transform(self.model.vectorize(self.binary_mol))
         latent=latent.squeeze(0)
         smiles, _ = self.model.predict(latent, temp=0)
         # The same output molecule can have several corresponding smiles and the decoder is trained
